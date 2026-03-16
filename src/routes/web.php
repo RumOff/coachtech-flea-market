d3f6e1c841f5604cf5dc2ab14e395cd9a6802ae7
RumOff <rum_sakamoto@icloud.com>
2026-03-16 23:28:56 +0900

    feat:ユーザー認証新規会員登録、ログイン画面作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+
+Route::middleware('auth')->group(function (){
+    Route::get('/', [AuthController::class, 'index']);
+
+
+
+    // プロフィール
+    Route::get('/mypage', function () {
+        return view('mypage.index');
+    });
+    Route::get('/mypage/profile', function () {
+        return view('mypage.profile');
+    });
+
+    // 商品購入
+    Route::get('/purchase/{item_id}', function () {
+        return view('purchase.index');
+    });
+    Route::get('/purchase/address/{item_id}', function () {
+        return view('purchase.address');
+    });
+
+
+
+
+});
 
 Route::get('/', [ProductController::class, 'index']);
 Route::get('/item/{item_id}', [ProductController::class, 'show']);
@@ -10,7 +37,6 @@ Route::get('/sell', function () {
     return view('sell');
 });
 
-
 Route::get('/register', function () {
     return view('auth.register');
 });
@@ -18,18 +44,3 @@ Route::get('/login', function () {
     return view('auth.login');
 });
 
-// プロフィール
-Route::get('/mypage', function () {
-    return view('mypage.index');
-});
-Route::get('/mypage/profile', function () {
-    return view('mypage.profile');
-});
-
-// 商品購入
-Route::get('/purchase/{item_id}', function () {
-    return view('purchase.index');
-});
-Route::get('/purchase/address/{item_id}', function () {
-    return view('purchase.address');
-});
